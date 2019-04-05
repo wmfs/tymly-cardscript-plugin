54aa1665966cd70359bb4ff229910d6f24b21cd0
Jez Higgins <jez@jezuk.co.uk>
2019-04-05 14:51:48 +0100

    test: Use todo id generated by db

--- test/todo-spec.js
+++ test/todo-spec.js
@@ -12,9 +12,6 @@ const GET_TODO_CHANGES_STATE_MACHINE = 'tymly_getTodoChanges_1_0'
 const CREATE_TO_DO_ENTRY = 'tymly_createTodoEntry_1_0'
 const REMOVE_TODO_STATE_MACHINE = 'tymly_removeTodoEntries_1_0'
 
-const ID_1 = '5200987c-bb03-11e7-abc4-cec278b6b111'
-const ID_2 = '5200987c-bb03-11e7-abc4-cec278b6b50a'
-
 describe('todo changes tymly-cardscript-plugin tests', function () {
   this.timeout(process.env.TIMEOUT || 5000)
   let statebox, todos, tymlyService, client
@@ -41,6 +38,7 @@ describe('todo changes tymly-cardscript-plugin tests', function () {
   })
 
   describe('user todo entry', () => {
+    let userTodoId = null
 
     it('create a user todo entry', async () => {
       const executionDescription = await statebox.startExecution(
@@ -48,8 +46,7 @@ describe('todo changes tymly-cardscript-plugin tests', function () {
           todoTitle: 'ToDo Expense Claim',
           stateMachineTitle: 'Process expense claim for User',
           stateMachineCategory: 'Expenses',
-          description: 'Claiming $12 for A pack of Duff Beer',
-          id: ID_1
+          description: 'Claiming $12 for A pack of Duff Beer'
         },
         CREATE_TO_DO_ENTRY,
         {
@@ -62,10 +59,12 @@ describe('todo changes tymly-cardscript-plugin tests', function () {
       expect(executionDescription.currentResource).to.eql('module:createTodoEntry')
       expect(executionDescription.stateMachineName).to.eql(CREATE_TO_DO_ENTRY)
       expect(executionDescription.status).to.eql('SUCCEEDED')
+
+      userTodoId = executionDescription.ctx.idProperties.id
     })
 
     it('todo is present', async () => {
-      const doc = await todos.findById(ID_1)
+      const doc = await todos.findById(userTodoId)
       expect(doc.userId).to.eql('todo-user')
       expect(doc.description).to.eql('Claiming $12 for A pack of Duff Beer')
     })
@@ -77,7 +76,7 @@ describe('todo changes tymly-cardscript-plugin tests', function () {
           stateMachineTitle: 'Process expense claim for User',
           stateMachineCategory: 'Expenses',
           description: 'User is claiming $12 for A pack of Duff Beer',
-          id: ID_1
+          id: userTodoId
         },
         CREATE_TO_DO_ENTRY,
         {
@@ -93,7 +92,7 @@ describe('todo changes tymly-cardscript-plugin tests', function () {
     })
 
     it('todo is updated', async () => {
-      const doc = await todos.findById(ID_1)
+      const doc = await todos.findById(userTodoId)
       expect(doc.userId).to.eql('todo-user')
       expect(doc.description).to.eql('User is claiming $12 for A pack of Duff Beer')
     })
@@ -101,7 +100,7 @@ describe('todo changes tymly-cardscript-plugin tests', function () {
     it('remove the todo', async () => {
       await statebox.startExecution(
         {
-          todoId: ID_1
+          todoId: userTodoId
         },
         REMOVE_TODO_STATE_MACHINE,
         {
@@ -112,7 +111,7 @@ describe('todo changes tymly-cardscript-plugin tests', function () {
     })
 
     it('todo is removed', async () => {
-      const doc = await todos.findById(ID_1)
+      const doc = await todos.findById(userTodoId)
       expect(doc).to.eql(undefined)
     })
   })
@@ -141,7 +140,7 @@ describe('todo changes tymly-cardscript-plugin tests', function () {
       expect(executionDescription.status).to.eql('SUCCEEDED')
       expect(Object.keys(executionDescription.ctx.todoChanges.add).length).to.eql(2)
       expect(Object.keys(executionDescription.ctx.todoChanges.add)
-        .includes(ID_2)).to.eql(true)
+        .includes('5200987c-bb03-11e7-abc4-cec278b6b50a')).to.eql(true)
       expect(Object.keys(executionDescription.ctx.todoChanges.add)
         .includes('0d625558-ce99-11e7-b7e3-c38932399c15')).to.eql(true)
       expect(executionDescription.ctx.todoChanges.remove).to.eql([])
@@ -151,7 +150,7 @@ describe('todo changes tymly-cardscript-plugin tests', function () {
       const executionDescription = await statebox.startExecution(
         {
           clientTodos: [
-            ID_2,
+            '5200987c-bb03-11e7-abc4-cec278b6b50a',
             '52009d36-bb03-11e7-abc4-cec278b6b50a',
             '52009e4e-bb03-11e7-abc4-cec278b6b50a',
             '52009f20-bb03-11e7-abc4-cec278b6b50a',
